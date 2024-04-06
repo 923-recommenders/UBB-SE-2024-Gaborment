f8b8c093149717cd618647ed8d39858cb200820f
IoanaGabor <ioana.gabor.cj@gmail.com>
2024-04-06 12:51:06 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace UBB_SE_2024_Gaborment
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
