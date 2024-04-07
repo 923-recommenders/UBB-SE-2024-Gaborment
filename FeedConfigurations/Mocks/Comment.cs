57c3c354eaa748d8c7f9c9366e8c4a2e82c6d085
Olivia <olivia.fintina@yahoo.com>
2024-04-07 17:27:15 +0300

    TG-46, TG-48, TG-49, TG-50, TG-80, TG-81

---
+++ FeedConfigurations/Mocks/Comment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedConfigurations.Mocks
+{
+    public class Comment
+    {
+        User Owner { get; set; }
+        String Text { get; set; }
+
+        public Comment(User Owner, String Text)
+        {
+            this.Owner = Owner;
+            this.Text = Text;
+        }
+
+        public Comment()
+        {
+            this.Owner = null;
+            this.Text = "";
+        }
+
+        public String getOwner()
+        {
+            return Owner.GetUsername();
+        }
+
+        public String getText()
+        {
+            return Text;
+        }
+    }
+}
--- FeedConfigurations/Mocks/User.cs
+++ FeedConfigurations/Mocks/User.cs
@@ -4,11 +4,28 @@ using System.Linq;
 using System.Text;
 using UBB_SE_2024_Gaborment.FeedConfigurations;
 
-namespace UBB_SE_2024_Gaborment.FeedConfigurations.Mocks
+namespace FeedConfigurations.Mocks
 {
     public class User
     {
+        Guid ID { get; set; }
         String Username { get; set; }
+
+        public User(String username)
+        {
+            this.ID = Guid.NewGuid();
+            this.Username = username;
+        }
+
+        public String GetUsername()
+        {
+            return Username;
+        }
+
+       public void SetUsername(String username)
+        {
+            Username = username;
+        }
 
     }
 }
