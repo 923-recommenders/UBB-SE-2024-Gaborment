af76c2c618c81b7a03a78af371c4f9670460c13e
Daria-Georgiana Feneșan <geo505georgiana@gmail.com>
2024-04-07 18:27:34 +0300

    TG-63, TG-59, TG-135: Implemented FeedConfigurationService class with CRUD operations

---
+++ Server/FeedConfigurations/FeedConfigurationService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UBB_SE_2024_Gaborment.FeedRepo;
+
+namespace UBB_SE_2024_Gaborment.Server.FeedConfigurationService
+{
+    public class FeedConfigurationService
+    {
+        private FeedConfigurationRepository applicationContext;
+
+        public FeedConfigurationService()
+        {
+            this.applicationContext = new FeedConfigurationRepository();
+        }
+
+        public FeedConfigurationService(FeedConfigurationRepository applicationContext)
+        {
+            this.applicationContext = applicationContext;
+        }
+
+        public List<FeedConfiguration> getFeedList()
+        {
+            return this.applicationContext.GetFeedList();
+        }
+
+        public FeedConfiguration getFeed(int id)
+        {
+            return this.applicationContext.getFeed(id);
+        }
+
+        public void addCustomFeed(string[]? hashtagsList, string[]? locations, string[]? users)
+        {
+            this.applicationContext.addCustomFeedToRepository(hashtagsList, locations, users);
+        }
+
+        public void updateCustomFeed(int id, string[]? hashtagsList, string[]? locations, string[]? users)
+        {
+            this.applicationContext.updateCustomFeed(id, hashtagsList, locations, users);
+        }
+
+        public void deleteCustomFeed(int id)
+        {
+            this.applicationContext.deleteCustomFeed(id);
+        }
+    }
+}
