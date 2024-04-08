f3eaabe5b2930fcb49571c6a0c5e55276f41c755
sequinnox <115191104+FintinaOlivia@users.noreply.github.com>
2024-04-08 02:26:42 -0700

    Revert "Feed configurations"

--- Server/Relationships/Follow/FollowService.cs
+++ Server/Relationships/Follow/FollowService.cs
@@ -60,20 +60,6 @@ namespace UBB_SE_2024_Gaborment.Server.Relationships.Follow
             return _followRepository.getFollowersOf(sender);
         }
 
-        public List<String> getCloseFriendsOf(string sender)
-        {
-            foreach (Follow follow in _followRepository.getFollowersOf(sender))
-            {
-                if (follow.getCloseFriendStatus() == true)
-                {
-                    return follow.getReceiver();
-                }
-            }
-            //return _followRepository.getFollowersOf(sender)
-            //                        .filter( f => f.getCloseFriendStatus() == true)
-            //                        .getSender();
-        }
-
         public List<Follow> getFollowingOf(string receiver)
         {
             return _followRepository.getFollowingOf(receiver);
