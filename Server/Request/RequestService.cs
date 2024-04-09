7d6bfebaa727740a82c18d209c02ed87aa5ee66a
OCTAVIAN-MIHAIL GHEORGHE <octavian.gheorghe@stud.ubbcluj.ro>
2024-04-09 18:47:39 +0300

    updated-friendship-management-backend

--- Server/Request/RequestService.cs
+++ Server/Request/RequestService.cs
@@ -33,12 +33,9 @@ namespace UBB_SE_2024_Gaborment.Server.Request
             return _requestRepository;
         }
 
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
         public void createRequest(string sender, string receiver)
         {
-            //!!!!!!!!!
-            //check if the set sender-receiver is located in a follow or a block from up until now. If it is, then the operation is not completed
+           
             if (!(_blockService.getBlocksBy(sender).Any(b => b.getReceiver() == receiver) || _blockService.getBlocksOf(receiver).Any(b => b.getSender() == sender) || _followService.getFollowersOf(sender).Any(f => f.getReceiver() == receiver) || _requestRepository.GetRequestsOf(sender).Any(r => r.getReceiver() == receiver)))
             {
                 UserMock newUser = _userServiceMock.GetUserById(sender);
@@ -77,8 +74,6 @@ namespace UBB_SE_2024_Gaborment.Server.Request
             return _requestRepository.GetRequestsOf(sender).Select(request => request.getReceiver()).ToList();
         }
 
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        ///
 
         public List<UserMock> getRequestOfAsUserList(string sender)
         {
