1beaf929799db11552d5908bf38c498eb3cbc5be
OCTAVIAN-MIHAIL GHEORGHE <octavian.gheorghe@stud.ubbcluj.ro>
2024-04-10 04:34:10 +0300

    Enabled functionality for friend list management

--- MVVM/ViewModel/FriendViewModel.cs
+++ MVVM/ViewModel/FriendViewModel.cs
@@ -11,6 +11,8 @@ using UBB_SE_2024_Gaborment.Server;
 using UBB_SE_2024_Gaborment.Server.Mocks;
 using UBB_SE_2024_Gaborment.Server.Relationships.Follow;
 using Microsoft.VisualBasic.ApplicationServices;
+using System.Windows.Input;
+
 
 namespace UBB_SE_2024_Gaborment.MVVM.ViewModel
 {
@@ -18,20 +20,57 @@ namespace UBB_SE_2024_Gaborment.MVVM.ViewModel
     {
         public ObservableCollection<UserMock> Users { get; set; }
 
+        private string _currentState;
+        public string CurrentState
+        {
+            get { return _currentState; }
+            set
+            {
+                _currentState = value;
+                LoadUsers();
+            }
+        }
+
         public FriendViewModel()
         {
             Users = new ObservableCollection<UserMock>();
-            LoadUsers();
+            CurrentState = "Friends"; // Set the initial state
         }
 
         public void LoadUsers()
         {
             ApplicationService service = ApplicationService.Instance;
             List<UserMock> users = new List<UserMock>();
-            
-            users = service.getPeopleUserIsFollowing("1");
-            Users = new ObservableCollection<UserMock>(users);
+
+            switch (CurrentState)
+            {
+                case "Friends":
+                    IEnumerable<UserMock> commonUsers;
+                    commonUsers = service.getPeopleUserIsFollowing("1").AsQueryable().Intersect(service.getPeopleUserIsBeingFollowedBy("1"));
+                    users = commonUsers.ToList();
+                    break;
+                case "Following":
+                    users = service.getPeopleUserIsFollowing("1");
+                    break;
+                case "Followers":
+                    users = service.getPeopleUserIsBeingFollowedBy("1");
+                    break;
+                case "Blocked":
+                    users = service.getPeopleUserBlocked("1");
+                    break;
+                case "Requests":
+                    users = service.getRequestsUserSent("1");
+                    break;
+                default:
+                    break;
+            }
+
+            Users.Clear();
+            foreach (var user in users)
+            {
+                Users.Add(user);
+            }
         }
     }
-}
 
+}
