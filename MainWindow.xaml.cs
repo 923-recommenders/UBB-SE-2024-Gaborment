062951961f948f3ed489c52c5c44b4f1fe65ba2f
GeanovuMedeea <medeea.geanovu@gmail.com>
2024-04-12 18:48:21 +0300

    Changed the fetching of posts to look repeatedly 3 days back

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,9 +11,6 @@ using UBB_SE_2024_Gaborment.MVVM.ViewModel;
 
 namespace UBB_SE_2024_Gaborment
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
